bbe2e26530a8c00fdd1e37b1a98b18c78f536d4e
maitaixiaojie <214518413@qq.com>
2020-04-15 22:13:34 +0800

    添加register.js

---
+++ src/Pages/register.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../assets/css/home.css'
+import $ from 'jquery'
+export default class Register extends React.Component{
+    constructor(props){
+       super(props)
+    }
+   
+    render(){
+        return(
+           <div className="register">
+             
+           </div>
+        )
+    }
+}
